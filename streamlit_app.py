2c12c891fd677ed95de65c74bf693400a4ac4069
Raja Telagatoti <rajatelagathoti19@gmail.com>
2026-07-06 12:26:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
-import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+<!doctype html>
+<html>
+<head><title>Gharda Chemicals</title></head>
+<body>
+  <h1>Gharda Chemicals Pvt. Ltd.</h1>
+  <h4>Kindly enter your details below</h4>
+  <form action = "/submit" method = "POST">
+    <label for = "name">Enter your Name: </label>
+    <input type = "text" name = "name" id = "name" placeholder = "Ex: Raja"></br>
+    <label for = "number">Enter your Whatsapp Number: </label>
+    <input type = "number" name = "phnumber" id = "phnumber" placeholder = "Enter your Whatsapp Number"></br>
+    <label for = "village">Enter your Village Name: </label>
+    <input type = "text" name = "village" id = "village" placeholder="Ex: Kandukur"></br>
+    <button type = "submit">Submit</button>
+  </form>
+</body>
+</html>
